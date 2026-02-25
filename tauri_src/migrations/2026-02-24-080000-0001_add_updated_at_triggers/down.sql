0c71cdb71e39172fdb112ccaad8b0cf96232ea2a
Joe Graff <graffj98@gmail.com>
2026-02-24 20:05:45 -0700

    Add update trigger

---
+++ tauri_src/migrations/2026-02-24-080000-0001_add_updated_at_triggers/down.sql
@@ -0,0 +1,6 @@
+DROP TRIGGER IF EXISTS projects_updated_at;
+DROP TRIGGER IF EXISTS events_updated_at;
+DROP TRIGGER IF EXISTS issues_updated_at;
+DROP TRIGGER IF EXISTS reviews_updated_at;
+DROP TRIGGER IF EXISTS composite_file_review_state_updated_at;
+DROP TRIGGER IF EXISTS branch_context_updated_at;
